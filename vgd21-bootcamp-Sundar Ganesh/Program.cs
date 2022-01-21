ea31501623809b23bb1b643b5b184cc31bc4a732
MUKILTEO\1710136 <1710136@mukilteo.wednet.edu>
2022-01-21 08:20:34 -0800

    Add project files.

---
+++ vgd21-bootcamp-Sundar Ganesh/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace vgd21_bootcamp_Sundar_Ganesh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
